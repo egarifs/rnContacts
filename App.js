35b5be058e343096314a9f2285e72fae25baadaf
firdaus <firdaus@pop-os.localdomain>
2021-03-27 17:34:50 +0700

    fist commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import AppNavContainer from './src/navigation';
+
+const App = () => {
+  return <AppNavContainer />;
+};
+
+export default App;
